b9b3f7fc9fd40576bbba527e4fffc5ed26e4f63d
Ole Strohm <strohm99@gmail.com>
2024-09-23 20:53:01 +0100

    It talks on embassy!

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
 #![no_main]
 #![no_std]
 
-use defmt_rtt as _;
-use nrf52840_hal as _;
+use embassy_executor::Spawner;
+use {defmt_rtt as _, panic_probe as _, cortex_m_rt as _};
 
-#[panic_handler]
-fn panic(_: &core::panic::PanicInfo) -> ! {
-    defmt::error!("panic!");
-    exit();
-}
-
-#[cortex_m_rt::entry]
-fn main() -> ! {
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let _ = embassy_nrf::init(Default::default());
     defmt::println!("Hello, world!");
-
-    exit();
-}
-
-fn exit() -> ! {
-    cortex_m::interrupt::disable();
-    loop {
-        cortex_m::asm::wfi()
-    }
 }
